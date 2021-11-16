82c70ecd53306aa153978ba7234cb2019d2f9d78
thewilkupl <thewilku@o2.pl>
2021-11-17 00:06:18 +0100

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+*,*::before, *::after{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-export default MyApp
+html{
+  font-family: 'Rubik', sans-serif;
+}
+`;
+
+const theme = {
+  colors: {
+    BlueColor: "hsl(246, 80%, 60%)",
+    LightRedColor: "hsl(15, 100%, 70%)",
+    Softblue: "hsl(195, 74%, 62%)",
+    Lightred: "hsl(348, 100%, 68%)",
+    Limegreen: "hsl(145, 58%, 55%)",
+    Violet: "hsl(264, 64%, 52%)",
+    Softorange: "hsl(43, 84%, 65%)",
+    VeryDarkBlue: "hsl(226, 43%, 10%)",
+    DarkBlue: "hsl(235, 46%, 20%)",
+    Desaturatedblue: "hsl(235, 45%, 61%)",
+    PaleBlue: "hsl(236, 100%, 87%)",
+  },
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,58 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      // eslint-disable-next-line no-param-reassign
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap" rel="stylesheet"></link>
+
+        </Head>
+        <body className="debug-screens">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
