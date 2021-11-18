faeec0919c91e26eda23b0cf35c61bc6b7137e12
thewilkupl <thewilku@o2.pl>
2021-11-18 17:20:57 +0100

    icon fixv4

--- src/utils/returnIcons.tsx
+++ src/utils/returnIcons.tsx
@@ -6,20 +6,18 @@ import IconSocial from "../../images/icon-social.svg";
 import IconStudy from "../../images/icon-study.svg";
 
 const icons = [
-  <IconWork key="1" />,
-  <IconExercise key="2" />,
-  <IconPlay key="3" />,
-  <IconSelfCare key="4" />,
-  <IconSocial key="5" />,
-  <IconStudy key="6" />,
+  <IconWork key="1" tag="work" />,
+  <IconExercise key="2" tag="exercise" />,
+  <IconPlay key="3" tag="play" />,
+  <IconSelfCare key="4" tag="selfcare" />,
+  <IconSocial key="5" tag="social" />,
+  <IconStudy key="6" tag="study" />,
 ];
 
 export const returnIcon = (iconName: string) => {
-  console.log(iconName);
-  console.log(icons);
 
   const icon = icons.find((item) =>
-    item.type.name.includes(iconName.split(" ").join(""))
+    item.props.tag.includes(iconName.split(" ").join("").toLowerCase())
   );
 
   if (icon) {
