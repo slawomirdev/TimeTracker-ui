22c7c09f79194611cee4980c1806e44d9337faf0
thewilkupl <thewilku@o2.pl>
2021-11-18 16:03:14 +0100

    create util function to return proper icon

---
+++ src/utils/returnIcons.tsx
@@ -0,0 +1,27 @@
+import IconWork from "../../images/icon-work.svg";
+import IconExercise from "../../images/icon-exercise.svg";
+import IconPlay from "../../images/icon-play.svg";
+import IconSelfCare from "../../images/icon-self-care.svg";
+import IconSocial from "../../images/icon-social.svg";
+import IconStudy from "../../images/icon-study.svg";
+
+const icons = [
+  <IconWork key="1" />,
+  <IconExercise key="2" />,
+  <IconPlay key="3" />,
+  <IconSelfCare key="4" />,
+  <IconSocial key="5" />,
+  <IconStudy key="6" />,
+];
+
+export const returnIcon = (iconName: string) => {
+  const icon = icons.find((item) =>
+    item.type.name.includes(iconName.split(" ").join(""))
+  );
+
+  if (icon) {
+    return icon;
+  } else {
+    return <IconWork />;
+  }
+};
